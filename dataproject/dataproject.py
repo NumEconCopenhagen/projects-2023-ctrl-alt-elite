6d2f159acfe163aac2879e9019835d64b30d672c
Emiknd <110400884+Emiknd@users.noreply.github.com>
2023-04-11 15:51:53 +0200

    Api2

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,4 +1,8 @@
 #Collecting data through Energinet's API
+import requests
+import time
+import pandas as pd
+
 response = requests.get(
     url='https://api.energidataservice.dk/dataset/ProductionConsumptionSettlement?offset=0&start=2022-01-01T00:00&end=2023-01-01T00:00&sort=HourUTC%20DESC&timezone=dk'
 )
@@ -15,7 +19,7 @@ records = result.get('records', [])
 print('records:')
 for record in records:
     print(' ', record)
-    
+
 result.keys()
 type(result['records'])
 
