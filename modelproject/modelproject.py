a85f10be3e1895d1951f8cae7b157388cb2e96f3
Natacha01 <83702066+Natacha01@users.noreply.github.com>
2023-05-10 10:51:12 +0200

    Har jeg sagt jeg hader makro?

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,6 +1,6 @@
 from scipy import optimize
 import numpy as np
-from scipy import optimize
+import scipy.optimize as opt
 from scipy import linalg
 import sympy as sm
 from sympy.solvers import solve
@@ -36,11 +36,14 @@ class OilSolowModelClass():
         par.delta = sm.symbols('delta')
         par.beta = sm.symbols('beta')
         par.etha = sm.symbols('etha')
+        par.phi = sm.symbols('phi')
         par.s = sm.symbols('s')
+        par.se = sm.symbols('s_E')
         par.n = sm.symbols('n')
         par.g = sm.symbols('g')
         par.d = sm.symbols('D')
         par.dT = sm.symbols('dT')
+        par.R = sm.symbols('R')
 
         # model parameters
     
@@ -50,9 +53,14 @@ class OilSolowModelClass():
         val.delta = 0.05    # capital depreciation rate
         val.alpha = 0.2     # capital share of output
         val.g = 0.027    # technological progress rate
-        val.beta =  0.6 
-        val.etha = 0
-        val.phi =1
+        val.beta =  0.6   # labor share of output
+        val.etha = 0.2    # energy share of output
+        val.phi =1   # climate damage parameter
+        val.k_0 = 1         # initial capital stock
+        val.L_0 = 1         # initial labor force
+        val.A_0 = 1         # initial total factor productivity
+        val.R_0 = 1         # initial oil reserves
+
     
         #simulation parameters
         par.simT =100  # 100 perioder
@@ -63,6 +71,16 @@ class OilSolowModelClass():
         sim.L = np.zeros(par.simT) # Workforce
         sim.A = np.zeros(par.simT) # total factor productivit
         sim.Y = np.zeros(par.simT) # Output - GDP
+
+    def solve_steady_state(self):
+        val = self.val
+        par = self.par
+
+        # steady state values
+        par.k_star = (val.s * val.A_0 ** (1 - val.beta) * (val.L_0 * val.R_0) ** val.beta / (val.delta + val.g)) ** (1 / (1 - val.alpha - val.beta))
+        par.L_star = val.L_0 * (1 + val.n) ** (par.simT - 1)
+        par.A_star = val.A_0 * (1 + val.g) ** (par.simT - 1)
+        par.R
 
     # How does climate damages develop over time and how much og the production is lost do to this?
     def solve_climate_damage(self):
@@ -81,5 +99,6 @@ class OilSolowModelClass():
         R_t = R_0 * (1 - se) ** t
         D_t = 1 - ((R_0 * (1 - se) ** t) / R_0) ** phi
         return D_t
-    
+
+
 
