9b43ac0b6f27abab27f35c0fc0989d562963eee8
Natacha01 <83702066+Natacha01@users.noreply.github.com>
2023-05-13 13:19:34 +0200

    Hej heeej

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,3 +1,4 @@
+from multiprocessing import Value
 from pyexpat import model
 from scipy import optimize
 import numpy as np
@@ -49,7 +50,7 @@ class OilSolowModelClass():
 
         # model parameters
     
-        val.se = 0.005     # oil udvingings rate
+        val.s_E = 0.005     # oil udvingings rate
         val.s = 0.3     # savings rate
         val.n = 0.01    # population growth rate
         val.delta = 0.05    # capital depreciation rate
@@ -57,14 +58,15 @@ class OilSolowModelClass():
         val.g = 0.027    # technological progress rate
         val.beta =  0.6   # labor share of output
         val.etha = 0.2    # energy share of output
-        val.phi =0.5   # climate damage parameter
+        val.phi = 0.5   # climate damage parameter
         val.phi2 = 0
         val.k_0 = 1         # initial capital stock
         val.L_0 = 1         # initial labor force
         val.A_0 = 1         # initial total factor productivity
-        val.R_0 = 1         # initial oil reserves
+        val.R = 1         # initial oil reserves
         val.e = 1
         val.Y_0 = 1
+        val.D = 0
        
 
     
@@ -73,13 +75,200 @@ class OilSolowModelClass():
         sim.D = np.zeros(par.simT)  # climate economic damages 
         sim.E = np.zeros(par.simT) # Oil usage
         sim.R = np.zeros(par.simT) # Oil stock left
-        sim.k = np.zeros(par.simT) # Physical capital
+        sim.K = np.zeros(par.simT) # Physical capital
         sim.L = np.zeros(par.simT) # Workforce
         sim.A = np.zeros(par.simT) # total factor productivit
         sim.Y = np.zeros(par.simT) # Output - GDP
         sim.Phi = np.zeros(par.simT) # climate damage parameter
         sim.g = np.zeros(par.simT) # technological progress rate
         sim.z = np.zeros(par.simT)
+        sim.fracY = np.zeros(par.simT)
+        sim.fracYD = np.zeros(par.simT)
+        sim.fracYDgrowth = np.zeros(par.simT)
+        sim.fracY_ext = np.zeros(par.simT)
+
+    
+    def simulate1(self):
+        par = self.par
+        val = self.val
+        sim = self.sim
+        
+     # simulating without climate change
+     
+        val.phi = 0
+        for t in range(par.simT):
+            if t == 0: 
+             # setting the values for period 0
+             A_lag = 1
+             K_lag = 1 
+             L_lag = 1 
+             R_lag = 1 
+             D_lag = 1- (val.R /R_lag)**val.phi
+             E_lag = val.s_E*R_lag
+             Y_lag = (1 - D_lag) * K_lag** val.alpha *(A_lag*L_lag)**val.beta*E_lag**val.etha
+
+             #the model equations for period 0
+
+             L=sim.L[t] = L_lag
+             A=sim.A[t] = A_lag
+             K= sim.K[t] = K_lag
+             R = sim.R[t] =R_lag
+             D = sim.D[t] = 1- (val.R /R_lag)**val.phi
+             E =sim.E[t] = val.s_E* R_lag
+             Y = sim.Y[t] = (1-sim.D[t]) * sim.K[t]** val.alpha *(sim.A[t]*sim.L[t])**val.beta*sim.E[t]**val.etha
+
+        else:
+            # setting the lagged values from period t=1 to t=100
+             A_lag = sim.A[t-1]
+             K_lag = sim.K[t-1]
+             L_lag = sim.L[t-1]
+             R_lag = sim.R[t-1]
+             D_lag = sim.D[t-1]
+             E_lag = sim.E[t-1]
+             Y_lag = sim.Y[t-1]
+
+            # the model equations for period t = 1 to t = 100
+             A = sim.A[t] = (1+val.g)*A_lag
+             K = sim.K[t] = val.s*Y_lag +(1-val.delta)*K_lag
+             L = sim.L[t] = (1+val.n)*L_lag
+             R = sim.R[t] = R_lag - E_lag
+             D = sim.D[t] = 1- (val.R /R_lag)**val.phi
+             E = sim.E[t] = val.s_E* R_lag
+             Y = sim.Y[t] = 1-sim.D[t] * sim.K[t]** val.alpha *(sim.A[t]*sim.L[t])**val.beta*sim.E[t]**val.etha
+    
+        # calculating the relative growth in GDP
+        sim.fracY[t] = (sim.Y[t]/sim.L[t])/(sim.Y[0]/sim.L[0])
+    
+    def simulate2(self):
+        par = self.par
+        val = self.val
+        sim = self.sim
+        
+     # simulating without climate change
+     
+        val.phi = 0.5
+        for t in range(par.simT):
+            if t == 0: 
+             # setting the values for period 0
+             A_lag = 1
+             K_lag = 1 
+             L_lag = 1 
+             R_lag = 1 
+             D_lag = 1- (val.R /R_lag)**val.phi
+             E_lag = val.s_E*R_lag
+             Y_lag = (1 - D_lag) * K_lag** val.alpha *(A_lag*L_lag)**val.beta*E_lag**val.etha
+
+             #the model equations for period 0
+
+             L=sim.L[t] = L_lag
+             A=sim.A[t] = A_lag
+             K= sim.K[t] = K_lag
+             R = sim.R[t] =R_lag
+             D = sim.D[t] = 1- (val.R /R_lag)**val.phi
+             E =sim.E[t] = val.s_E* R_lag
+             Y = sim.Y[t] = (1-sim.D[t]) * sim.K[t]** val.alpha *(sim.A[t]*sim.L[t])**val.beta*sim.E[t]**val.etha
+
+        else:
+            # setting the lagged values from period t=1 to t=100
+             A_lag = sim.A[t-1]
+             K_lag = sim.K[t-1]
+             L_lag = sim.L[t-1]
+             R_lag = sim.R[t-1]
+             D_lag = sim.D[t-1]
+             E_lag = sim.E[t-1]
+             Y_lag = sim.Y[t-1]
+
+            # the model equations for period t = 1 to t = 100
+             A = sim.A[t] = (1+val.g)*A_lag
+             K = sim.K[t] = val.s*Y_lag +(1-val.delta)*K_lag
+             L = sim.L[t] = (1+val.n)*L_lag
+             R = sim.R[t] = R_lag - E_lag
+             D = sim.D[t] = 1- (val.R /R_lag)**val.phi
+             E = sim.E[t] = val.s_E* R_lag
+             Y = sim.Y[t] = 1-sim.D[t] * sim.K[t]** val.alpha *(sim.A[t]*sim.L[t])**val.beta*sim.E[t]**val.etha
+    
+        # calculating the relative growth in GDP with climate change  
+        sim.fracYD[t] = (sim.Y[t]/sim.L[t])/(sim.Y[0]/sim.L[0])
+    
+    def simulate3(self):
+        par = self.par
+        val = self.val
+        sim = self.sim
+        
+     # simulating without climate change
+     
+        val.phi = 1
+        for t in range(par.simT):
+            if t == 0: 
+             # setting the values for period 0
+             A_lag = 1
+             K_lag = 1 
+             L_lag = 1 
+             R_lag = 1 
+             D_lag = 1- (val.R /R_lag)**val.phi
+             E_lag = val.s_E*R_lag
+             Y_lag = (1 - D_lag) * K_lag** val.alpha *(A_lag*L_lag)**val.beta*E_lag**val.etha
+
+             #the model equations for period 0
+
+             L=sim.L[t] = L_lag
+             A=sim.A[t] = A_lag
+             K= sim.K[t] = K_lag
+             R = sim.R[t] =R_lag
+             D = sim.D[t] = 1- (val.R /R_lag)**val.phi
+             E =sim.E[t] = val.s_E* R_lag
+             Y = sim.Y[t] = (1-sim.D[t]) * sim.K[t]** val.alpha *(sim.A[t]*sim.L[t])**val.beta*sim.E[t]**val.etha
+
+        else:
+            # setting the lagged values from period t=1 to t=100
+             A_lag = sim.A[t-1]
+             K_lag = sim.K[t-1]
+             L_lag = sim.L[t-1]
+             R_lag = sim.R[t-1]
+             D_lag = sim.D[t-1]
+             E_lag = sim.E[t-1]
+             Y_lag = sim.Y[t-1]
+
+            # the model equations for period t = 1 to t = 100
+             A = sim.A[t] = (1+val.g)*A_lag
+             K = sim.K[t] = val.s*Y_lag +(1-val.delta)*K_lag
+             L = sim.L[t] = (1+val.n)*L_lag
+             R = sim.R[t] = R_lag - E_lag
+             D = sim.D[t] = 1- (val.R /R_lag)**val.phi
+             E = sim.E[t] = val.s_E* R_lag
+             Y = sim.Y[t] = 1-sim.D[t] * sim.K[t]** val.alpha *(sim.A[t]*sim.L[t])**val.beta*sim.E[t]**val.etha
+    
+          # calculating the relative growth in GDP with growing climate change 
+        sim.fracYDgrowth[t] = (sim.Y[t]/sim.L[t])/(sim.Y[0]/sim.L[0])
+
+
+
+
+    
+        
+
+        
+
+        
+
+        
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
 
     def solve_steady_state(self):
         val = self.val
@@ -138,44 +327,6 @@ class OilSolowModelClass():
         sum = balance_no_climate()
         print ("Phi= 0",balance_no_climate)
 
-    def solve_output(self): 
-        par = self.par
-        sim = self.sim
-        val = self.val
-
-        # Initial values
-        val.k_0 = 1.0
-        val.e_0 = 1.0
-        val.A_0 = 1.0
-        val.Y_0 = sim.Y[t] = (1 - sim.D[t]) * sim.k[t] ** val.alpha * sim.A[t] ** val.beta * val.e ** val.etha 
-
-        # Number of periods to simulate
-        T = 100
-
-        #         Arrays to store the results
-        sim.k = np.zeros(T+1)
-        sim.A = np.zeros(T+1)
-        sim.Y = np.zeros(T+1)
-
-        sim.k[0] = val.k_0
-        sim.L[0] = val.L_0
-        sim.A[0] = val.A_0
-        sim.Y[0] = val.Y_0
-
-        # Simulate the model
-        for t in range(T):
-            sim.Y[t] = sim.A[t] * (sim.k[t]**val.alpha) * (sim.L[t]**(1-val.alpha))
-            sim.k[t+1] = val.s * sim.Y[t] * (1-val.delta) + (1-val.delta) * sim.k[t]
-            sim.L[t+1] = (1+val.n) * sim.L[t]
-            sim.A[t+1] = (1+val.g) * sim.A[t]
-
-        # Plot the output over time
-        fig, ax = plt.subplots()
-        ax.plot(range(T+1), sim.Y)
-        ax.set_xlabel('Time')
-        ax.set_ylabel('Output')
-        ax.set_title('Output over Time')
-        plt.show()
 
 
-      
+
