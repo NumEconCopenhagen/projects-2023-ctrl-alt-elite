bfd7c8175f0cf361591c185da7f43f1a3a01e890
Natacha01 <83702066+Natacha01@users.noreply.github.com>
2023-05-23 10:48:49 +0200

    Fixxxxxxxin

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -3,31 +3,30 @@ import requests
 import time
 import pandas as pd
 
-response = requests.get(
-    url='https://api.energidataservice.dk/dataset/ProductionConsumptionSettlement?offset=0&start=2022-01-01T00:00&end=2023-01-01T00:00&sort=HourUTC%20DESC&timezone=dk'
-)
+# Send a GET request to the API and importing the data
+url='https://api.energidataservice.dk/dataset/ProductionConsumptionSettlement?offset=0&start=2022-01-01T00:00&end=2023-01-01T00:00&sort=HourUTC%20DESC&timezone=dk'
+params = {
+    'offset': 0,
+    'start': '2022-01-01T00:00',
+    'end': '2023-01-01T00:00',
+    'sort': 'HourUTC DESC',
+    'timezone': 'dk'
+}
+response = requests.get(url, params=params)
 
+# Parse the JSON response
 result = response.json()
 
 # Limit the output to a certain number of records
 limit = 10
 records = result.get('records', [])[:limit]
 
-for record in records:
-    print(' ', record)
-
-for k, v in result.items():
-    print(k, v)
-
-    time.sleep(20)
-
-records = result.get('records', [])
-
+# Extract keys from the result
 result.keys()
-type(result['records'])
+
+# Check the type of records
+record_type = type(result['records'])
 
 # Convert to DataFrame
 df = pd.DataFrame(result['records'])
 
-# Display the first few rows of the DataFrame
-print(df.head())
