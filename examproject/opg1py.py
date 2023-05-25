c78d54289bf8903ed95f94e2278a666294b8abda
Emiknd <110400884+Emiknd@users.noreply.github.com>
2023-05-25 16:37:14 +0200

    Q1.3

--- examproject/opg1py.py
+++ examproject/opg1py.py
@@ -26,16 +26,16 @@ class ConsModel():
         par = self.par
         val = self.val
 
-        par.L = sm.symbols('L', positive=True)
-        par.C = sm.symbols('C')
+        par.L = sm.symbols('L', nonnegative=True)
+        par.C = sm.symbols('C', nonnegative=True)
         par.G = sm.symbols('G', positive=True)
 
-        par.alpha = sm.symbols('alpha')
+        par.alpha = sm.symbols('alpha', nonnegative=True)
         par.kappa = sm.symbols('kappa', positive=True)
         par.v = sm.symbols('v', positive=True)
         par.w = sm.symbols('w', positive=True)
-        par.tau = sm.symbols('tau')
-        par.wtilde = sm.symbols('wtilde')
+        par.tau = sm.symbols('tau', nonnegative=True)
+        par.wtilde = sm.symbols('wtilde', positive=True)
 
     def solve_analytical(self):
         par = self.par
@@ -57,8 +57,6 @@ class ConsModel():
         # Solve the first-order condition
         optimal_L = sm.solve(eq, par.L)
 
-        # Print the optimal labor input
-        #print("Optimal Labor Input (L):", optimal_L)
         return optimal_L
     
     def plot1(self):
@@ -83,6 +81,79 @@ class ConsModel():
         plt.ylabel('L')
         plt.title('Optimal Labor Input as a Function of w')
         plt.grid(True)
+        plt.show()
+
+    def plot2(self):
+
+        par = self.par
+
+        # We set the values of the tax close to 0 and 1 as the model cannot solve for tau = 0 and tau = 1
+        tau_values = np.linspace(0.001,0.999,100)
+
+        # Create an empty list to store results
+        optimal_L_values =[]
+        G_func_values =[]
+        V_func_values =[]
+        C_func_values =[]
+
+        par.alpha = 0.5
+        par.kappa = 1
+        par.v = 1/(2*(16**2))
+        par.w = 1
+
+        # Iterate over tau values
+        for tau in tau_values:
+            par.tau = tau
+            par.wtilde = (1 - par.tau) * par.w
+            
+            optimal_L = self.solve_analytical()
+            optimal_L_values.append(optimal_L[0])
+
+            G_func = par.tau * par.w * optimal_L[0] * par.wtilde
+            G_func_values.append(G_func)
+
+            C_func = par.kappa+par.wtilde*optimal_L[0]
+            C_func_values.append(C_func)
+
+            V_func = sm.log(C_func**par.alpha * G_func **(1 - par.alpha)) - par.v * (optimal_L[0]**2) / 2
+            V_func_values.append(V_func)
+
+        # Plot optimal labor input as a function of tau
+        plt.subplot(311)
+        plt.plot(tau_values, optimal_L_values)
+        plt.xlim(0, 1)
+        plt.xlabel('tau')
+        plt.ylabel('L')
+        plt.title('Optimal labor input as a function of tau')
+        plt.grid(True)
+
+        # Plot G_func as a function of tau
+        plt.subplot(312)
+        plt.plot(tau_values, G_func_values)
+        plt.xlim(0, 1)
+        plt.xlabel('tau')
+        plt.ylabel('G')
+        plt.title('Government spenditure as a function of tau')
+        plt.grid(True)
+
+        # Plot V_func as a function of tau
+        plt.subplot(313)
+        plt.plot(tau_values, V_func_values)
+        plt.xlim(0, 1)
+        plt.xlabel('tau')
+        plt.ylabel('V')
+        plt.title('Utility as a function of tau')
+        plt.grid(True)
+
+        # Adjust the spacing between subplots
+        plt.subplots_adjust(wspace=0.5, hspace=0.5)
+
+        # Set the figure size
+        fig = plt.gcf()
+        fig.set_size_inches(8, 12)
+
+        # Display the plots
+        plt.tight_layout()
         plt.show()
  
 
